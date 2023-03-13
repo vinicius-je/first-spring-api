34d72344e4bb59ed21e032fd108237ddd07f3f84
vinicius-je <vinicius.je99@gmail.com>
2023-03-13 18:04:49 -0300

    find by id

--- src/main/java/com/example/cardapio/controller/FoodController.java
+++ src/main/java/com/example/cardapio/controller/FoodController.java
@@ -5,9 +5,11 @@ import com.example.cardapio.food.FoodRepository;
 import com.example.cardapio.food.FoodRequestDTO;
 import com.example.cardapio.food.FoodResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("food")
@@ -24,6 +26,12 @@ public class FoodController {
     public List<FoodResponseDTO> getAll(){
         List<FoodResponseDTO> foodList = repository.findAll().stream().map(FoodResponseDTO::new).toList();
         return foodList;
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Food> getById(@PathVariable Long id){
+        Optional<Food> food = repository.findById(id);
+        return food;
     }
 
 
